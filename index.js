5c78e96f94903a63a969ace3ac389d450ab43c99
xS4yk0x <sayko117@gmail.com>
2023-04-30 17:04:18 +0300

    Simple API to load data.json

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const http = require("http");
-// const fs = require("fs");
+const fs = require("fs");
 
 // fs.readFile(`./txt/input.txt`, "utf-8", (err, data) => {
 //   console.log(data);
 // });
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  //Loading the data synchronously
+  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+  const dataObj = JSON.parse(data);
 
+  //Reading the request path
+  const pathName = req.url;
   if (pathName === "/product") {
     res.end("Welcome to the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end(`<h1> Page not found! </h1>`);
