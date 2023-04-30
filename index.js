bd8e4b2664ee0a9063f83476989a2318661ca7ab
xS4yk0x <sayko117@gmail.com>
2023-04-30 16:23:08 +0300

    Basic node server config

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const fs = require("fs");
+const http = require("http");
+// const fs = require("fs");
 
-fs.readFile(`./txt/input.txt`, "utf-8", (err, data) => {
-  console.log(data);
+// fs.readFile(`./txt/input.txt`, "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log(`Listening to requests on port 8000...`);
 });
