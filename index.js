bb951ec412c9579611ff8a88fe1c8feb0cf7405d
xS4yk0x <sayko117@gmail.com>
2023-04-30 18:52:56 +0300

    Loading the html pages & showing overview

--- index.js
+++ index.js
@@ -10,13 +10,55 @@ const server = http.createServer((req, res) => {
   const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
   const dataObj = JSON.parse(data);
 
+  // Loading template html files and replace the data
+
+  const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    "utf-8"
+  );
+  const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+  );
+  const tempProdcut = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
+  );
+
+  // function that replace data within the template
+  const replaceTemplate = (template, product) => {
+    let finalTemplate = template.replace(
+      /{%PRODUCTNAME%}/g,
+      product.productName
+    );
+    finalTemplate = finalTemplate.replace(/{%IMAGE%}/g, product.image);
+    finalTemplate = finalTemplate.replace(/{%PRICE%}/g, product.price);
+    finalTemplate = finalTemplate.replace(
+      /{%DESCRIPTION%}/g,
+      product.description
+    );
+    finalTemplate = finalTemplate.replace(/{%QUANTITY%}/g, product.quantity);
+    finalTemplate = finalTemplate.replace(/{%ID%}/g, product.id);
+    if (!product.organic)
+      finalTemplate = finalTemplate.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+    return finalTemplate;
+  };
+
   //Reading the request path
   const pathName = req.url;
 
   // Prdocut page
   if (pathName === "/overview" || pathName === "/") {
   } else if (pathName === "/product") {
-    res.end("Welcome to the product");
+    const htmlCards = dataObj
+      .map((product) => replaceTemplate(tempCard, product))
+      .join("");
+    const htmlOverview = tempOverview.replace(`{%PRODUCT_CARDS%}`, htmlCards);
+
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    res.end(htmlOverview);
 
     // API page
   } else if (pathName === "/api") {
