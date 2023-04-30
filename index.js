7aaa36942affc54f356b429afe212c8c384808aa
xS4yk0x <sayko117@gmail.com>
2023-04-30 16:41:39 +0300

    basic routes

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const http = require("http");
 // });
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/product") {
+    res.end("Welcome to the product");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end(`<h1> Page not found! </h1>`);
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
